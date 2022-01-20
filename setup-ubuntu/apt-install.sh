743f9e5d72a3a0dac523ca4a12d281282fa28fc2
Kai Luo <kluophysics@gmail.com>
2022-01-20 13:13:59 +0800

    adding path and more apt-install

--- setup-ubuntu/apt-install.sh
+++ setup-ubuntu/apt-install.sh
@@ -4,14 +4,14 @@
 sudo apt update
 sudo apt -y upgrade
 sudo apt -y install build-essential
-sudo apt -y install vim
-sudo apt -y install git
+sudo apt -y install gcc gfortran g++
+sudo apt -y install git git-gui
 sudo apt -y install tree
 
+sudo apt -y install vim vim-gtk3
 sudo apt -y install cmake
 sudo apt -y install zsh
 sudo apt -y install autojump
-sudo apt -y install gcc gfortran g++
 
 
 # math libraries
@@ -21,7 +21,6 @@ sudo apt -y install liblapack-dev liblapacke-dev libblas-dev libblas64-dev libff
 
 
 # common softwares
-
 # vlc
 sudo apt -y install vlc 
 
@@ -37,15 +36,21 @@ sudo apt -y install vlc
 sudo wget -O - https://tagplus5.github.io/vscode-ppa/ubuntu/gpg.key | sudo apt-key add - && \
 sudo wget -O /etc/apt/sources.list.d/vscode.list https://tagplus5.github.io/vscode-ppa/ubuntu/vscode.list && \
 sudo apt update &&
-sudo apt install code code-insiders
+sudo apt -y install code code-insiders
 
+# libreoffice suite
+sudo apt -y install libreoffice libreoffice-texmaths
 
 # texlive-latex
-
 sudo apt -y install texlive-latex-base texlive-latex-recommended texlive-publishers texlive-luatex texlive-pictures texlive-lang-chinese texlive-fonts-recommended texlive-fonts-extra texlive-bibtex-extra texlive-xetex texlive-pstricks texlive-metapost
 
 # gnuplot 
 sudo apt -y install gnuplot-qt
-
 # vncviewer
 sudo apt -y install tigervnc-viewer
+# gimp
+sudo apt -y install gimp
+# inkscape
+sudo apt -y install inkscape
+# 
+
